1407fb037de2be634c025faefa7790f82ec8399f
chitswe <chitswe.mdy@gmail.com>
2019-02-19 16:40:19 +0630

    endpoint

--- src/server/odoo.ts
+++ src/server/odoo.ts
@@ -8,9 +8,10 @@ type Odoo = {
     options?: any
   ) => Promise<any>;
 };
+const env = process.env.NODE_ENV ? process.env.NODE_ENV : "production";
 const Odoo = (creditional: { username: string; password: string }) => {
   const oodoo = new oOdoo({
-    url: "http://odoo.mt.com.mm",
+    url: env === "production" ? "http://127.0.0.1:80" : "http://odoo.mt.com.mm",
     db: "Tri-Treasure",
     username: creditional.username,
     password: creditional.password
